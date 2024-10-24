dc84a07c0fc1c3a27fa85efc2fb1749454bd6b66
M.Ihtisham <146211746+ChIhtisham78@users.noreply.github.com>
2024-10-24 15:57:27 +0500

    Add project files.

---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentManagement.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Email { get; set; }
+		public string? PhoneNumber { get; set; }
+		public string? Address { get; set; }
+		public string? Country { get; set; }
+	}
+}
