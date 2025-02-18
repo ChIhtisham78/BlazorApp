94f95faf3f94badd256977239248b6684ad7ed28
M.Ihtisham <146211746+ChIhtisham78@users.noreply.github.com>
2025-02-18 17:01:47 +0500

    Update changes

--- StudentManagement.Shared/Models/Student.cs
+++ StudentManagement.Shared/Models/Student.cs
@@ -1,13 +1,16 @@
-﻿namespace StudentManagement.Shared.Models
-{
-	public class Student
-	{
-		public int Id { get; set; }
-		public string? Name { get; set; }
-		public string? Email { get; set; }
-		public string? PhoneNumber { get; set; }
-		public string? Address { get; set; }
-		public string? Country { get; set; }
+﻿using System.ComponentModel.DataAnnotations;
 
-	}
+namespace StudentManagement.Shared.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Country { get; set; }
+        public string? Address { get; set; }
+
+    }
 }
